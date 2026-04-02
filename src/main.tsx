b3949dfe27368ae7bd71e5e9035d04e40ffdd862
Niharika Sunwar <niharika@Yogini.localdomain>
2026-04-02 18:05:00 +0100

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar.tsx'
+import { AppSidebar } from './components/app-siderbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <App />
+      </main>
+    </SidebarProvider>
   </StrictMode>,
 )
